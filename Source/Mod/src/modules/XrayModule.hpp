b271a43ffefcf946cf5605788bb9fb7463d07c3b
jwarren5683-max <jwarren5683@gmail.com>
2026-09-15 19:04:39 -0500

    Revert blocking 26.50 ESP/X-ray adapters to restore menu startup

--- Source/Mod/src/modules/XrayModule.hpp
+++ Source/Mod/src/modules/XrayModule.hpp
@@ -33,7 +33,6 @@ public:
     void on_register(EventBus&) override;
     void on_enable() override;
     void on_disable() override;
-    void on_tick() noexcept override;
 
     [[nodiscard]] static constexpr bool valuable(std::string_view identifier) noexcept {
         for (const auto ore : ores) {
@@ -96,4 +95,3 @@ private:
 };
 
 } // namespace utility::modules
-
